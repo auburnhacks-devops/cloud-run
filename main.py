e95743ed3a7e7856cad6ca968a7d0e92a1ee9a65
kkalla <kkalla@tsys.com>
2021-04-09 12:25:23 -0400

    gcp test

--- main.py
+++ main.py
@@ -1,33 +1,43 @@
-# Copyright 2020 Google, LLC.
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#    http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-
-# [START cloudrun_helloworld_service]
-# [START run_helloworld_service]
-import os
-
-from flask import Flask
-
+import csv
+import lib.pandas as pd
+import requests
+from lib.flask import Flask,render_template,request,redirect,Response,flash,url_for
+#from lib.flask_table import Table, Col
+import os     
+import sys
+from datetime import datetime
+CSV_URL = 'https://api.covidtracking.com/v1/states/current.csv'
+data=pd.read_csv(CSV_URL)
+#print(data.head(1))
+#cursor = connection.cursor()  
+#app=Flask(__name__, template_folder='templates/')
+#if getattr(sys, 'frozen', False):
+    #template_folder = os.path.join(sys._MEIPASS, 'templates')
+    #static_folder = os.path.join(sys._MEIPASS, 'static')
+    #app = Flask(__name__, template_folder=template_folder, static_folder=static_folder)
+#else:
 app = Flask(__name__)
+#ui = FlaskUI(app)
 
+#app.config['']
+@app.route('/')
+def Index():
+    return render_template('index.html')
 
-@app.route("/")
-def hello_world():
-    name = os.environ.get("NAME", "World")
-    return "Hello {}!".format(name)
+@app.route('/info',methods=['POST','GET'])
+def info():
+    if request.method == 'POST':
+        Sname = request.form['Sname']
+        print(Sname)
+        Dbase = request.form['Dbase']
+        data1=data[(data['state']=='FL') & (data['date']==20210307)]   
+        #table = Table(data1)     
+        return  render_template('view.html',tables=[data1.to_html(classes='female')],
+    titles = ['na', 'Covid Imporamation for '+Sname])
+    else:
+        return("Bad request please verify the input values.")
+#Response (data1.to_json(orient='split'))
 
-
-if __name__ == "__main__":
+if __name__ =="__main__":
+    app.secret_key = os.urandom(24)
     app.run(debug=True, host="0.0.0.0", port=int(os.environ.get("PORT", 8080)))
-# [END run_helloworld_service]
-# [END cloudrun_helloworld_service]
